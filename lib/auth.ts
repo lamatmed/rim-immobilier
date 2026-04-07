d6195ba0591e13a64ff2e1f7f70ca6116da81241
Lamat Abdellahi <lamat032025@gmail.com>
2026-04-08 00:14:04 +0100

    first commit

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { SignJWT, jwtVerify } from "jose";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
